1ddd3001a9f7e64b2e866f138025045a5d4dd262
timggraf <tim.g.graf@gmail.com>
2020-12-21 11:16:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
+import pandas as pd
 
-if _name_ == "_main_":
-  pass
+df = pd.read_csv('/Users/tgraf/Google Drive/Uni SG/Master/Smart Data Analytics/00 Group Project/Repository/Single-Timeseries-Crypto-Bot/Data/400 - 1m - Trading Pairs (2013-2020)/btcusd.csv')
