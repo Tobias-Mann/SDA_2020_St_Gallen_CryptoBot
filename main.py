90a28cd2f7ff6cddfff4758c2d0442dbe800a167
timggraf <tim.g.graf@gmail.com>
2020-12-21 11:27:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pandas as pd
 
+# import dataframe
 df = pd.read_csv('/Users/tgraf/Google Drive/Uni SG/Master/Smart Data Analytics/00 Group Project/Repository/Single-Timeseries-Crypto-Bot/Data/400 - 1m - Trading Pairs (2013-2020)/btcusd.csv')
+print(df.info())
+
+# convert values from timestamp to date
+print(df['time'])
+pd.to_datetime(df['time'])
+
+
+pd.Timestamp(df['time'])
+df['time'][2]
+
+'{:.20f}'.format(df['time'])
