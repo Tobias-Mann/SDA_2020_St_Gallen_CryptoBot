81719482aba70adf7438dc3c8016e0ee76742c56
timggraf <tim.g.graf@gmail.com>
2020-12-21 18:40:36 +0100

    Update data import.py

--- data import.py
+++ data import.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt # plotting
 import numpy as np # linear algebra
 import os # accessing directory structure
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from functools import reduce
+import pyfolio as pf # for technial analysis
+from zipline.api import order, record, symbol
+
+
 
 
 ### DATA SOURCE 1: KAGGLE ----------------------------
@@ -29,6 +34,24 @@ print(df2['unix'])
 
 ### DATA SOURCE 3: GITHUB  ----------------------------
 #https://github.com/Zombie-3000/Bitfinex-historical-data
-# Please download the data manually as it is too large for the repo
 
-df3 = pd.read_csv('/Users/tgraf/Google Drive/Uni SG/Master/Smart Data Analytics/00 Group Project/Data/Bitfinex-historical-data-master/BTCUSD/Candles_1m/2013')
+headers = ['time', 'open', 'close', 'highest', 'lowest', 'volume']
+df_13 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2013/merged.csv?raw=true', names = headers)
+df_14 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2014/merged.csv?raw=true', names = headers)
+df_15 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2015/merged.csv?raw=true', names = headers)
+df_16 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2016/merged.csv?raw=true', names = headers)
+df_17 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2017/merged.csv?raw=true', names = headers)
+df_18 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2018/merged.csv?raw=true', names = headers)
+df_19 = pd.read_csv('https://raw.githubusercontent.com/Zombie-3000/Bitfinex-historical-data/master/BTCUSD/Candles_1m/2019/merged.csv?raw=true', names = headers)
+
+# Merge dataframes
+data_frames = [df_13, df_14, df_15, df_16, df_17, df_18, df_19]
+df_merged = pd.concat(data_frames)
+
+#convert timestamp to datae
+df_merged['time'] = pd.to_datetime(df_merged['time'], unit = 'ms')
+
+# Write csv of merged files
+pd.DataFrame.to_csv(df_merged, 'Zoombie_merged.txt', sep=',', na_rep='.', index=False)
+
+# we should have a df with 6'832'800 rows (13y*365d*24h*60m)but only have 2'630'217 rows 
