32dcad709ac42134508d3b0a93c5d6ccf16a0824
Tobias Mann <62596507+Tobias-Mann@users.noreply.github.com>
2020-12-26 02:47:05 +0100

    Fix simulator
    Includes finished drafsts for a) orderbook and b) transaction book
    c) the simulator can now process orders from the orderbook (for each price) and keep track of all occured transactions

--- simulator.py
+++ simulator.py
@@ -33,18 +33,39 @@ class orderbook:
         if status in ["active", "filled", "canceled"]:
             self.all_orders[id]["status"] = status
     
-    def new_marketorder(self, is_buy = True):
+    def new_marketorder(self, quantity, is_buy = True):
         id = self.__assign_id__()
         self.__new__order__()
-        self.all_orders[id] = {"type": "market", "is_buy":is_buy, "status":"active", "id":id}
+        self.all_orders[id] = {"type": "market", "is_buy":is_buy, "quantity":quantity, "status":"active", "id":id}
         
-    def new_limitorder(self, price, is_buy = True):
+    def new_limitorder(self, price, quantity, is_buy = True):
         id = self.__assign_id__()
         self.__new__order__()
-        self.all_orders[id] = {"type": "limit", "is_buy":is_buy, "status":"active", "id":id, "limit": price}
+        self.all_orders[id] = {"type": "limit", "is_buy":is_buy, "quantity":quantity, "status":"active", "id":id, "limit": price}
     
 class transactionbook:
     def __init__(self):
+        # The Transaction book is based on a dictionary of transactions
+        # each transaction contains a quantity {int}, price:{decimal}, time:{datetime}, is_buy{True, False}, id: {int}, origin{int}
+        self.transactions = dict{}
+        self.__last_id__ = 0
+    
+    def __assign_id__(self):
+        self.__last_id__+=1
+        return self.__last_id__
+    
+    def buy(self,price, qyt, time, origin):
+        id = self.__assign_id__()
+        self.transactions[id] = {"quantity":qyt, "price":price, "is_buy":True, "id":id, "time":time, "origin":origin}
+    
+    def sell(self,price, qyt, time, origin):
+        id = self.__assign_id__()
+        self.transactions[id] = {"quantity":qyt, "price":price, "is_buy":False, "id":id, "time":time, "origin":origin}
+    
+    def get_buys(self):
+        pass
+    
+    def get_sells(self):
         pass
 
 class simulator:
@@ -61,16 +82,38 @@ class simulator:
         weights = randoms/randoms.sum()
         return ((np.array([ open, high, low, close]) * weights).sum()*2 + open + close)/4
     
-    def observe_new_ohlc(self, ohcl):
-        # Check for open orders to be filled 
+    def process_orders(self, time, ohcl):
+        # Check for open orders to be filled
+        # A filled order needs to change its status from active to filled,
+        # aditionally an according transaction is added to the transactions book
         for order in self.orderbook.orders_by_status("active").values():
+            filled = False
+            
+            # update the transactionbook for trades
             if order["is_buy"]:
-                pass
+                if order["type"]=="market":
+                    # buy at market
+                    price = self.random_market_price(ohcl[0], ohcl[1], ohcl[2], ohcl[3])
+                    self.transactionbook.buy(price, order["quantity"], time, order["id"])
+                    filled = True
+                elif order["limit"] >= ohcl[2]:
+                    # buy at limit
+                    self.transactionbook.buy(order["limit"], order["quantity"], time, order["id"])
+                    filled = True
             else:
-                pass
-        
-        # update the transactionbook for trades
-        # update the orderbook for trades
+                if order["type"]=="market":
+                    # sell at market
+                    price = self.random_market_price(ohcl[0], ohcl[1], ohcl[2], ohcl[3])
+                    self.transactionbook.sell(price, order["quantity"], time, order["id"])
+                    filled = True
+                elif order["limit"] <= ohcl[1]:
+                    # sell at limit
+                    self.transactionbook.sell(order["limit"], order["quantity"], time, order["id"])
+                    filled = True
+                    
+            # update the orderbook for trades
+            if filled:
+                self.orderbook.change_order_status(order["id"], "filled")
         
 
 class decisionmaker:
