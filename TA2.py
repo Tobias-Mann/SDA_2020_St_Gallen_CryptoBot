d1b9a9437204f7d9c4252691aadecd1c301cc729
timggraf <tim.g.graf@gmail.com>
2020-12-26 09:30:42 +0100

    upload data

--- TA2.py
+++ TA2.py
@@ -264,7 +264,7 @@ fig = plt.figure(figsize = (10, 10))
 ax1 = fig.add_subplot(111, ylabel='Portfolio value in $')
 
 # Plot the equity curve in dollars
-portfolio['total'].plot(ax=ax1, lw=2.)
+portfolio['total'].plot(ax = ax1, lw=2.)
 
 # Plot the "buy" trades against the equity curve
 ax1.plot(portfolio.loc[signals.positions == 1.0].index,
