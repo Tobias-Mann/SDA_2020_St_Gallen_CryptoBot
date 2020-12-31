33bf4012c1b46fb2cf62865116a7ca536f301af6
Tobias Mann <62596507+Tobias-Mann@users.noreply.github.com>
2020-12-31 15:06:48 +0100

    Fix RSI

--- simulator_test.py
+++ simulator_test.py
@@ -7,15 +7,15 @@ import strategies
 # create simulator
 sim = simulator.simulator_environment()
 #initialize decision maker
-sim.initialize_decisionmaker(strategies.SimpleMA)
+sim.initialize_decisionmaker(strategies.relativestrength)
 
 # read in data
 data = pd.read_csv("./Data/Dec19.csv")
 
 # start simulation --> This will take some time!!!
-sim.simulate_on_aggregate_data(data.dropna().head(200000))
+sim.simulate_on_aggregate_data(data.dropna())
 
 # retrieve Portfolio performance over simulated time
 print(sim.env.portfolio.portfolio_over_time)
-print(sim.decisionmaker.long_memory)
+print(sim.decisionmaker.rsi_memory)
 
