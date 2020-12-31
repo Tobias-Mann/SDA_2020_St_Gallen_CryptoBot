f34d2701268f689dbbbf04f4b0ffcd4f581687ec
timggraf <tim.g.graf@gmail.com>
2020-12-31 12:55:14 +0100

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -129,21 +129,24 @@ class relativestrength(simulator.decisionmaker):
         closing_price = row[-1]
         if len(self.memory) <= 1 or closing_price != self.memory[-1]:
             self.memory.append(closing_price)
-        values = self.memory[-(self.period+1):]
-        returns = np.diff(values)/values[:-1]
-        returns = returns[~np.isnan(returns)]
-        select = returns>0
-        avg_gain = np.mean(returns[select])
-        avg_loss = np.mean(returns[~select])
-        rsi = 100
-        if avg_loss != 0 and np.any(~np.isnan([avg_gain, avg_loss])):
-            rsi = 100 - (100 / (1 + avg_gain/avg_loss))
-        self.rsi_memory.append(rsi)
-        if rsi >= self.overbought:
-            # sell at market
-            quantity = self.env.portfolio.btc
-            self.env.orderbook.new_marketorder(quantity, False)
-        elif rsi <= self.oversold:
-            # buy at market
-            quantity = self.env.portfolio.usd//closing_price
-            self.env.orderbook.new_marketorder(quantity)
+        
+        if len(self.memory) >= self.period:
+            values = self.memory[-(self.period+1):]
+            returns = np.diff(values)/values[:-1]
+            returns = returns[~np.isnan(returns)]
+            select = returns>0
+            avg_gain = np.mean(returns[select])
+            avg_loss = np.mean(returns[~select])
+            rsi = 100
+            if avg_loss != 0 and np.any(~np.isnan([avg_gain, avg_loss])):
+                rsi = 100 - (100 / (1 + avg_gain/avg_loss))
+                self.rsi_memory.append(rsi)
+
+            if rsi >= self.overbought:
+                # sell at market
+                quantity = self.env.portfolio.btc
+                self.env.orderbook.new_marketorder(quantity, False)
+            elif rsi <= self.oversold:
+                # buy at market
+                quantity = self.env.portfolio.usd//closing_price
+                self.env.orderbook.new_marketorder(quantity)
