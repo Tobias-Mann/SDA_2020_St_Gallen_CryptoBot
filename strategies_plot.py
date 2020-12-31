012daa0b8d91411da471c6d35f3bc1c87d656b9b
timggraf <tim.g.graf@gmail.com>
2020-12-31 17:51:46 +0100

    Update strategies_plot.py

--- strategies_plot.py
+++ strategies_plot.py
@@ -1,13 +1,14 @@
 import matplotlib as plt
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Data Import --------------------------
 
 df = pd.read_csv('Data/strategies.csv')
 
 
-# Moving Averages--------------------------
+# Simple Moving Averages--------------------------
 
 short_window = 12
 long_window = 26
@@ -32,6 +33,7 @@ fig = plt.figure(num=None,
                  edgecolor='k')
 # Add a subplot and label for y-axis
 ax1 = fig.add_subplot(111, ylabel='Price in $')
+#ax1.margins(x=-0.4, y=--0.4)
 # Plot the closing price
 df['price'].plot(ax=ax1, color='r', lw=2.)
 
@@ -60,4 +62,47 @@ plt.show()
 fast = 12
 slow = 26
 signal = 9
-# df['signal_point'] = 0.0
+
+df['signal_point'] = 0.0
+
+# Create signals, 1.0 = Signal, 0.0 = No signal
+df['signal_point'][slow:] = np.where(
+    df['signal'][slow:] > df['macd'][slow:], 1.0, 0.0)
+
+# Generate trading orders
+df['positions'] = df['signal_point'].diff()
+df.loc[df['positions'] == 1]
+df.loc[df['positions'] == 0]
+df.loc[df['positions'] == -1]
+# 0 = do nothing, 1 = Buy, 2 = Sell
+
+# Initialize the plot figure
+fig = plt.figure(num=None,
+                 figsize=(10, 10),
+                 dpi=80,
+                 facecolor='w',
+                 edgecolor='k')
+# Add a subplot and label for y-axis
+ax1 = fig.add_subplot(111, ylabel='Price in $')
+# Plot the closing price
+df['price'].plot(ax=ax1, color='r', lw=2.)
+
+# Plot the short and long moving averages
+df[['signal', 'macd']].plot(ax=ax1, lw=2.)
+
+# Plot the buy signals
+ax1.plot(df.loc[df.positions == 1.0].index,
+         df.short_ma[df.positions == 1.0],
+         '^',
+         markersize=10,
+         color='m')
+
+# Plot the sell signals
+ax1.plot(df.loc[df.positions == -1.0].index,
+         df.short_ma[df.positions == -1.0],
+         'v',
+         markersize=10,
+         color='k')
+
+# Show the plot
+plt.show()
