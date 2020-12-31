4069f5ab63a8c3868a27b5aff87c405712addf2c
timggraf <tim.g.graf@gmail.com>
2020-12-31 13:45:05 +0100

    Update compare_strategies.py

--- compare_strategies.py
+++ compare_strategies.py
@@ -28,3 +28,35 @@ def dataframebycolumn(column):
 
 df_cumulative = dataframebycolumn("cumreturn")
 df_Absolute = dataframebycolumn("value")
+
+
+# merge function to get NaN at the beginning
+def merge_basedonlength(df1, df2, column_name):
+    len1 = len(df1)
+    len2 = len(df2)
+    df1[column_name] = np.NaN
+    df1[column_name][-len2:] = df2.iloc[:, 0]
+
+# create dataframes
+df = pd.DataFrame(memories['MACD'].memory, columns=['price'])
+df['time'] = data['time']
+df_macd = pd.DataFrame(memories['MACD'].macd_memory, columns=['macd'])
+df_signal = pd.DataFrame(memories['MACD'].signal_memory, columns=['signal'])
+df_short = pd.DataFrame(memories['SimpleMA'].short_memory,
+                        columns=['short_ma'])
+df_long = pd.DataFrame(memories['SimpleMA'].long_memory, columns=['long_ma'])
+df_rsi = pd.DataFrame(memories['RSI'].rsi_memory, columns=['rsi'])
+df_z = pd.DataFrame(memories['meanreversion'].z_memory, columns=['z_value'])
+
+# merge dataframes and save them
+merge_basedonlength(df, df_macd, 'macd')
+merge_basedonlength(df, df_signal, 'signal')
+merge_basedonlength(df, df_short, 'short_ma')
+merge_basedonlength(df, df_long, 'long_ma')
+merge_basedonlength(df, df_z, 'z_value')
+merge_basedonlength(df, df_rsi, 'rsi')
+
+print(df)
+
+# save dataframe
+df.to_csv('Data/strategies.csv')
