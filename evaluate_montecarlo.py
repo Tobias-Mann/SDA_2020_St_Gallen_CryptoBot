e6445f6cc321a1e158021464bfe58c7ad1971ac8
Tobias Mann <62596507+Tobias-Mann@users.noreply.github.com>
2021-01-02 19:03:47 +0100

    Stat tests

--- evaluate_montecarlo.py
+++ evaluate_montecarlo.py
@@ -3,12 +3,20 @@ import pandas as pd
 from scipy import stats
 
 
-def test_average_performance(paths, data):
+def test_average_performance(paths, data, threshold = .01):
     # This functin calculates the avgerage cumulative BTC return, and calculates the p-Value that the true cumulative simulation return is equal or below to BTC performance
     cumreturn=lambda x: x[-1]/x[0]-1
     btc = cumreturn(data.close.values)
     sumulation_cumreturns = paths.iloc[-1,:].values
-    t = (btc - sumulation_cumreturns.mean()) / sumulation_cumreturns.std()
+    t, p = stats.ttest_1samp(sumulation_cumreturns, btc)
+    if btc > sumulation_cumreturns.mean():
+        print(f"The strategy appears to perform worse on average than simply holding BTC")
+    elif p < threshold:
+        print(f"The t-Statistic for the simulations true cumulative return being identical to the the one of BTC is {round(t,4)}, the according p-Values is {round(p*100, 2)}% and below the {int(100*threshold)}% threshold!")
+    else:
+        print(f"The hypothesis of a significantly different performance is rejected at the {round(threshold*100,0)}% significance level")
+    
+def test_volatility(paths, data, threshold = .01):
     
 
 if __name__ == "__main__":
@@ -16,6 +24,7 @@ if __name__ == "__main__":
         if os.path.exists("./Data/BTC_USD/Dec19.csv"):
             data = pd.read_csv("./Data/BTC_USD/Dec19.csv")
             paths = pd.read_csv("./Data/lastmontecarlosimulation.csv").set_index("time")
+            test_average_performance(paths, data)
         else:
             print("File is mising: ./Data/BTC_USD/Dec19.csv")
     else:
