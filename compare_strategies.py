890702098e502438713894557843591f5c78a930
timggraf <tim.g.graf@gmail.com>
2021-01-02 15:19:59 +0100

    automate comparison

--- compare_strategies.py
+++ compare_strategies.py
@@ -4,16 +4,30 @@ from pandas import DataFrame
 import simulator
 import strategies
 import os
+from os import listdir
+from os.path import isfile, join
 
 
-STRATEGIESCOLLECTION = {"SimpleMA":strategies.SimpleMA, "MACD":strategies.MACD,
-    "RSI":strategies.relativestrength, "meanreversion":strategies.meanreversion}
 
-# read in data
+# SETUP ---------------------------------------------
+# Read in data
 data = pd.read_csv("./Data/BTC_USD/Dec19.csv")
 data = data.dropna().head(200000)
 
-# simulate multiple strategies
+# Define variables
+TIMEPERIOD = 'Dec19'
+PATH_PFS = './Data/Portfolios/'
+PATH_STRATEGIES = './Data/Strategies/'
+
+# Define stregies to be tested
+STRATEGIESCOLLECTION = {
+    "SimpleMA": strategies.SimpleMA,
+    "MACD": strategies.MACD,
+    "RSI": strategies.relativestrength,
+    "meanreversion": strategies.meanreversion
+}
+
+# SIMULATE MULTIPLE STRATEGIES --------------------------
 portfolios = {}
 memories = {}
 for name, strategy in STRATEGIESCOLLECTION.items():
@@ -34,13 +48,7 @@ def dataframebycolumn(column):
 df_cumulative = dataframebycolumn("cumreturn")
 df_Absolute = dataframebycolumn("value")
 
-
-# merge function to get NaN at the beginning
-def merge_basedonlength(df1, df2, column_name):
-    len1 = len(df1)
-    len2 = len(df2)
-    df1[column_name] = np.NaN
-    df1[column_name][-len2:] = df2.iloc[:, 0]
+### SAVE SHEETS FOR LATER ---------------------------------
 
 # Save Tearsheets
 def save_tearsheets (folder_time_name):
@@ -54,7 +62,7 @@ def save_tearsheets (folder_time_name):
         df = pd.DataFrame(portfolios[name].tearsheet(data))
         df.to_csv(folder + name_temp)
 
-save_tearsheets('Dec19')
+save_tearsheets(TIMEPERIOD)
 
 # Save Portfolios
 def save_portfolios (folder_time_name):
@@ -68,10 +76,44 @@ def save_portfolios (folder_time_name):
         df = pd.DataFrame(portfolios[name].portfolio_repricing(data))
         df.to_csv(folder + name_temp)
 
-save_portfolios('Dec19')
+save_portfolios(TIMEPERIOD)
+
+# get files with a .csv ending
+def find_csv_filenames(path_to_dir, suffix=".csv"):
+    filenames = listdir(path_to_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+filenames = find_csv_filenames(PATH_PFS + TIMEPERIOD + '/')
+
+# merge all files to one sheet
+def merge_cum_returns(path, filenames):
+    df = pd.DataFrame(index = data.index, columns = None)
+    df['time'] = data.time
+    count = 0
+    for name in filenames:
+        if filenames[count] == 'merged_cumreturn.csv':
+            count += 1
+        else:
+            df1 = pd.read_csv(path + filenames[count])
+            df[name] = df1['cumreturn']
+            count += 1
+    return df
+
+merged_cumreturns = merge_cum_returns(
+    (PATH_PFS + TIMEPERIOD + '/'), filenames)
+
+merged_cumreturns.to_csv(PATH_PFS + TIMEPERIOD + '/merged_cumreturn.csv')
+
+
+# merge function to get NaN at the beginning
+def merge_basedonlength(df1, df2, column_name):
+    len1 = len(df1)
+    len2 = len(df2)
+    df1[column_name] = np.NaN
+    df1[column_name][-len2:] = df2.iloc[:, 0]
 
 # create dataframes from memories
-def save_strategies (name):
+def save_strategies (name, folder_time_name):
 
     # get values
     df = pd.DataFrame(memories['SimpleMA'].memory, columns=['price'])
@@ -92,14 +134,15 @@ def save_strategies (name):
     merge_basedonlength(df, df_rsi, 'rsi')
 
     # save dataframe
-    folder = './Data/Strategies/' 
+    folder = './Data/Strategies/' + folder_time_name + '/'
     name_temp = name + '.csv'
 
     if os.path.isdir(folder):
         pass
     else:
-        os.mkdir('./Data/Strategies')
+        os.mkdir(folder)
 
     df.to_csv(folder + name_temp)
 
-save_strategies('Strategies_Dec19')
+strategies_name = 'Strategies_' + TIMEPERIOD
+save_strategies(strategies_name, TIMEPERIOD)
