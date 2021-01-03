f8dab92f1e78bd3ed49bd14b53204cca45d95a68
timggraf <tim.g.graf@gmail.com>
2021-01-03 22:24:48 +0100

    update 04

--- SDA_2020_St_Gallen_04_VisualizeMonteCarlo/SDA_2020_St_Gallen_04_VisualizeMonteCarlo.py
+++ SDA_2020_St_Gallen_04_VisualizeMonteCarlo/SDA_2020_St_Gallen_04_VisualizeMonteCarlo.py
@@ -7,7 +7,7 @@ import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
 import plotting
 
-"""
+
 def test_average_performance(paths, data, threshold = .01, verbose = False):
     # This functin calculates the avgerage cumulative BTC return, and calculates the p-Value that the true cumulative simulation return is equal or below to BTC performance
     cumreturn=lambda x: x[-1]/x[0]-1
@@ -31,23 +31,23 @@ if __name__ == "__main__":
             data = pd.read_csv(file)
             paths = pd.read_csv(paths_file).set_index("time")
             test_average_performance(paths, data, verbose=True)
-            plotting.create_mc_dist_plot(paths.reset_index(), data, (.9, .6), output="./Images/Dec19.png", title="Qlearning Monte Carlo Simulation vs BTC Dec19")
+            plotting.create_mc_dist_plot(paths.reset_index(), data, (.9, .6), output="./Dec19.png", title="Qlearning Monte Carlo Simulation vs BTC Dec19")
         else:
             print(f"File is mising: {file}")
     else:
         print("There is no data from a previous Monte Carlo Simulation. Please run first the simulation to generate the data")
-"""
+
 
 # MONTE CARLO SIMULATION PLOT ----------------------------------------------
 
-PATH_PLOTS = './Outputs'
+PATH_PLOTS = './'
 TIMEPERIOD = 'Dec_2019'
 FIGSIZE = (10,10)
 TIME_FMT = mdates.DateFormatter('%d-%m-%Y')
 
 
 # change the folder where the input is
-df_mc = pd.read_csv('..//SDA_2020_St_Gallen_02_Simulations/Output_Dec_2019/Dec19_MC_Paths.csv.gzip',  compression='gzip')
+df_mc = pd.read_csv('../SDA_2020_St_Gallen_02_Simulations/Output_Dec_2019/Dec19_MC_Paths.csv.gzip',  compression='gzip')
 df_mc['time'] = pd.to_datetime(df_mc['time'])
 df_mc_returns = df_mc.loc[:, df_mc.columns != 'time'].diff()
 
