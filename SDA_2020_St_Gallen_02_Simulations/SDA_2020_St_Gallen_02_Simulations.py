56414910169997926ef194a86ccd104c6065cfcc
Tobias Mann <62596507+Tobias-Mann@users.noreply.github.com>
2021-01-03 17:42:57 +0100

    Update

--- SDA_2020_St_Gallen_02_Simulations/SDA_2020_St_Gallen_02_Simulations.py
+++ SDA_2020_St_Gallen_02_Simulations/SDA_2020_St_Gallen_02_Simulations.py
@@ -133,10 +133,7 @@ merged_cumreturns = merge_dfs((PATH_PFS + TIMEPERIOD + '/'), filenames_pfs, 'cum
 merged_cumreturns['time'] = data['time']
 merged_cumreturns.to_csv(PATH_PFS + TIMEPERIOD + '/merged_cumreturn.csv')
 
-<<<<<<< Updated upstream:SDA_2020_St_Gallen_02_Simulations/SDA_2020_St_Gallen_02_Simulations.py
-=======
 
->>>>>>> Stashed changes:SDA_2020_St_Gallen_02_SimpleStratSim/SDA_2020_St_Gallen_02_SimpleStratSim.py
 # Strategies: save merged df
 def save_strategies (name):
 
